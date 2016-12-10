23c65b87feb1b62685efeab846712ca58a8aadb3
silverbearcsm <1813557113@qq.com>
2016-12-10 13:58:23 +0800

    login gui

--- src/Client/Client.java
+++ src/Client/Client.java
@@ -5,13 +5,13 @@ import java.net.*;
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
+
 import Status.*;
 
 public class Client extends JFrame{
 	private JTextField jtf=new JTextField();
 	private JTextArea jta=new JTextArea();
 	private JButton jbt=new JButton("view");
-	
 	
 	private ObjectOutputStream objtoServer;
 	private ObjectInputStream objfromServer;
@@ -22,8 +22,9 @@ public class Client extends JFrame{
 	}
 	
 	public Client(){
+		new LogIn();
 		initGui();
-		registListener();
+		registerListener();
 		connectToServer();
 	}
 	
@@ -44,11 +45,11 @@ public class Client extends JFrame{
 		setTitle("Client");
 		setSize(500,300);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setVisible(true);
+		setVisible(false);
 	}
 	
-	public void registListener(){
-		jbt.addActionListener(new TextFieldListener());
+	public void registerListener(){
+		jbt.addActionListener(new WordSearchListener());
 	}
 	
 	public void connectToServer(){
@@ -65,7 +66,7 @@ public class Client extends JFrame{
 		}
 	}
 	
-	private class TextFieldListener implements ActionListener{
+	private class WordSearchListener implements ActionListener{
 
 		@Override
 		public void actionPerformed(ActionEvent e) {
@@ -79,6 +80,61 @@ public class Client extends JFrame{
 				System.err.println(ex);
 			}
 		}
+	}
+	
+	
+	class LogIn extends JFrame{
+		private JTextField jtfNameField=new JTextField();
+		private JTextField jtfPassWordField=new JTextField();
+		private JButton jbtLogInButton=new JButton("Log in");
+		
+		public LogIn(){
+			setLogInGui();
+			registerlogInListener();
+		}
+		
+		public void setLogInGui(){
+			JPanel p1=new JPanel();
+			p1.setLayout(new BorderLayout());
+			p1.add(new JLabel("Enter Your Name"),BorderLayout.WEST);
+			p1.add(jtfNameField,BorderLayout.CENTER);
+			
+			JPanel p2=new JPanel();
+			p2.setLayout(new BorderLayout());
+			p2.add(new JLabel("Enter Your Password"),BorderLayout.WEST);
+			p2.add(jtfPassWordField,BorderLayout.CENTER);
+			
+			JPanel p3=new JPanel();
+			p3.setLayout(new BorderLayout());
+			p3.add(p1,BorderLayout.NORTH);
+			p3.add(p2,BorderLayout.SOUTH);
+			
+			setLayout(new BorderLayout());
+			setTitle("Log In");
+			setSize(240,180);
+			setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			setVisible(true);
+			
+			add(p3,BorderLayout.CENTER);
+			add(jbtLogInButton,BorderLayout.SOUTH);
+		}
+		
+		public void registerlogInListener(){
+			jbtLogInButton.addActionListener(new LogInListener());
+		}
+		
+		private class LogInListener implements ActionListener{
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				setVisible(false);
+				Client.this.setVisible(true);
+			}
+			
+		}
+
+	
 	}
 	
 }
