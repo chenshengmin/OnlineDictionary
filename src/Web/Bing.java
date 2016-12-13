40e93e8bc0c6af673d81c12b90bcb35baa39c24b
silverbearcsm <1813557113@qq.com>
2016-12-13 19:15:52 +0800

    web achieved

--- src/Web/Bing.java
+++ src/Web/Bing.java
@@ -1,7 +1,42 @@
 package Web;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+
 public class Bing {
 	public static String lookUp(String word){
-		return word;
+		java.util.Scanner input = null;
+		String explanation = null;
+		try {
+			URL url = new URL("http://cn.bing.com/dict/search?q=" +word);
+			input = new java.util.Scanner(url.openStream(),"UTF-8");
+			String startString="必应词典为您提供"+word+"的释义，";
+			while(input.hasNext()){
+				String s = input.nextLine();
+				int index=s.indexOf(startString);
+				if(index>=0){
+					//截取翻译部分到整行结尾
+					String temp=s.substring(index+startString.length(),s.length());
+					int endIndex=temp.indexOf("\"");
+					if(endIndex>=0){
+						//截取翻译部分
+						String content=temp.substring(0,endIndex);
+						explanation=content;
+						break;
+					}
+				}	
+			}
+			if(explanation!=null&&!explanation.equals(""))
+				explanation=word+"\n"+explanation;
+			return explanation;
+		} catch (MalformedURLException ex) {
+			// TODO Auto-generated catch block
+			ex.printStackTrace();
+		} catch(IOException ex){
+			ex.printStackTrace();
+		}
+		return null;
 	}
 }
--- src/Web/Youdao.java
+++ src/Web/Youdao.java
@@ -1,7 +1,62 @@
 package Web;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class Youdao {
 	public static String lookUp(String word){
-		return word;
+		java.util.Scanner input = null;
+		String explanation = "";
+		try {
+			URL url = new URL("http://dict.youdao.com/search?le=eng&q=" + word + "&keyfrom=dict.top");
+			input = new java.util.Scanner(url.openStream(),"UTF-8");
+			boolean explanBeginFlag = false; //开始中文翻译的标志
+			String pronType="<span class=\"pronounce\">";
+			String pronBegin="<span class=\"phonetic\">";
+			String pronEnd="</span>";
+			//获取音标
+			while(input.hasNext()){
+				String s = input.nextLine();
+				if(s.indexOf(pronType)>=0){
+					int begin = s.indexOf(pronType) + pronType.length();
+					int end = s.length();
+					explanation += s.subSequence(begin, end);
+				}
+				else if(s.indexOf(pronBegin)>=0){
+					int begin = s.indexOf(pronBegin) + pronBegin.length();
+					int end = s.indexOf(pronEnd);
+					explanation += s.subSequence(begin, end)+"\n";
+				}
+				else if(s.indexOf("<div class=\"trans-container\">")>=0){
+					break;
+				}
+			}
+			//获取中文翻译
+			while(input.hasNext()){
+				String s = input.nextLine();
+				if(s.equals("   <ul>"))
+					explanBeginFlag=true;
+				else if(!explanBeginFlag)
+					continue;
+				else if(s.equals("    </ul>"))
+					break;
+				else{
+					int begin = s.indexOf("<li>") + 4;
+					int end = s.indexOf("</li>");
+					explanation += s.subSequence(begin, end);
+					explanation += "\n";
+				}
+			}
+			if(explanation!=null&&!explanation.equals(""))
+				explanation=word+"\n"+explanation;
+			return explanation;
+		} catch (MalformedURLException ex){
+			// TODO Auto-generated catch block
+			ex.printStackTrace();
+		} catch(IOException ex){
+			ex.printStackTrace();
+		}
+		return null;
 	}
 }
